00969e479008d0819d06d0fc60fcc9dc702c03d5
piyushshrivastava01 <piyushsrivastava1009@gmail.com>
2025-06-08 01:31:29 +0530

    Todo task project 1 is accomplished

--- index.js
+++ index.js
@@ -1,16 +1,84 @@
-var allElems = document.querySelectorAll('.elem');
-var Fullelem = document.querySelectorAll('.fullElem')
-var backbtn = document.querySelectorAll('.fullElem .back')
- 
-allElems.forEach(function(elem){
-   elem.addEventListener('click', function(){
-      Fullelem[elem.id].style.display = 'block'
-   })  
-})
+function openfeature() {
 
+    var allElems = document.querySelectorAll('.elem');
+    var Fullelem = document.querySelectorAll('.fullElem')
+    var backbtn = document.querySelectorAll('.fullElem .back')
 
-backbtn.forEach(function(back){
-    back.addEventListener('click', function(){
-        Fullelem[back.id].style.display = 'none'
+    allElems.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            Fullelem[elem.id].style.display = 'block'
+        })
     })
-})
+
+
+    backbtn.forEach(function (back) {
+        back.addEventListener('click', function () {
+            Fullelem[back.id].style.display = 'none'
+        })
+    })
+}
+openfeature();
+
+function todolist() {
+
+    let form = document.querySelector('.addTask form');
+    let taskInput = document.querySelector(" .addTask form input");
+    let taskDetailsInput = document.querySelector(" .addTask form textarea");
+    let taskCheckbox = document.querySelector(" .addTask form #check");
+
+
+    var currentTask = []
+    if (localStorage.getItem('currentTask')) {
+        currentTask = JSON.parse(localStorage.getItem('currentTask'));
+    } else {
+        localStorage.setItem('currentTask', currentTask);
+    }
+
+    function rendertask() {
+
+        var allTask = document.querySelector('.allTask');
+        var sum = ''
+        currentTask.forEach(function (elem, idx) {
+            sum += `<div class="task">
+            <h5>${elem.task} <span class=${elem.imp}>imp</span></h5>
+            <button id=${idx}>Mark as Completed</button>
+            </div> `
+        })
+        allTask.innerHTML = sum;
+        localStorage.setItem('currentTask', JSON.stringify(currentTask));
+
+        var markCompletedBtn = document.querySelectorAll('.task button');
+
+        markCompletedBtn.forEach(function (btn) {
+            btn.addEventListener('click', function () {
+                currentTask.splice(btn.id, 1);
+                rendertask();
+                location.reload()
+            })
+        })
+    }
+    rendertask()
+
+
+    form.addEventListener('submit', function (e) {
+        e.preventDefault()
+        currentTask.push(
+            {
+                task: taskInput.value,
+                details: taskDetailsInput.value,
+                imp: taskCheckbox.checked
+            }
+        )
+        rendertask()
+        
+        taskCheckbox.checked = false
+        taskInput.value = ''
+        taskDetailsInput.value = ''
+
+    })
+
+
+}
+
+todolist();
+
